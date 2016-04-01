0cf99be4ac2f5d4974c402306e1e7e98c7ed362f
pearsonja <jonathan.pearson@willis.com>
2016-04-01 16:17:00 +0100

    merger2

--- shpDist.cpp
+++ shpDist.cpp
@@ -13,13 +13,9 @@ std::string number2string(int number){
 	return convert.str();
 }
 
-<<<<<<< HEAD
+
 std::string gen_hist_fn(int r, std::string prefix){
 	return "output/" + prefix + "_" + number2string(r) + ".dat";
-=======
-std::string gen_hist_fn(int r){
-	return "hist_" + number2string(r) + ".dat";
->>>>>>> d5975d2ff872bee0e34d30aa60a0cc452fac94cf
 }
 
 int main(){
@@ -29,7 +25,6 @@ int main(){
 	double rmax = 20;
 	
 	for(int r = 0; r < nreals; r++){
-<<<<<<< HEAD
         std::cout << "Computing realisation " << r + 1 << "/" << nreals << std::endl;
 	    CREATE_DIST dist(nlocs);
 		dist.set_rmax(rmax);
@@ -38,18 +33,7 @@ int main(){
 		dist.scan_distances();
 		dist.print_distance_stats();
 		dist.bin_distances( gen_hist_fn(r, "hist") );
-=======
-	    CREATE_DIST dist(nlocs);
-		dist.set_rmax(rmax);
-		std::cout << "* constructing distribution of points" << std::endl;
-	    dist.create_dist();
-		std::cout << "* computing distances between all points" << std::endl;
-		dist.scan_distances();
-		dist.print_distance_stats();
-		//dist.dump_distances("data.dat");
-		std::cout << "* binning distances between all points" << std::endl;
-		dist.bin_distances( gen_hist_fn(r) );
->>>>>>> d5975d2ff872bee0e34d30aa60a0cc452fac94cf
+
 	}	
 	
 }
